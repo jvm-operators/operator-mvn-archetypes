038d16fa405545947c80d700667da6e85a028feb
Jirka Kremser <jkremser@redhat.com>
2018-08-30 15:01:58 +0200

    tests: mvn archetype:generate has to be called in dir w/o a pom.xml

--- .travis/.travis.test.sh
+++ .travis/.travis.test.sh
@@ -5,12 +5,17 @@ set -xe
 main() {
   CURRENT=`cat pom.xml | grep "<version>" | sed 's/\s*<[^>]*>\s*//g'`
   echo "current version: ${CURRENT}"
+  mkdir dir-with-no-pom && cd dir-with-no-pom
+
+  # finally generate the project
   mvn archetype:generate \
      -DarchetypeArtifactId=operator-mvn-archetype \
      -DarchetypeGroupId=io.radanalytics \
      -DarchetypeVersion=${CURRENT} \
      -DinteractiveMode=false
   cd x-operator/
+
+  # try if it compiles
   mvn install
 }
 
