2e529906ec6ba1d0a8a367a4da39f1165672a1fe
Jirka Kremser <jkremser@redhat.com>
2018-08-30 14:38:09 +0200

    tests

--- .travis/.travis.test.sh
+++ .travis/.travis.test.sh
@@ -4,8 +4,11 @@ set -xe
 
 main() {
   CURRENT=`mvn -U org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev "(^\[|Download\w+:)"`
-  mvn archetype:generate -DarchetypeArtifactId=operator-mvn-archetype \
-     -DarchetypeGroupId=io.radanalytics -DarchetypeVersion=${CURRENT} \
+  echo "current version: ${CURRENT}"
+  mvn archetype:generate \
+     -DarchetypeArtifactId=operator-mvn-archetype \
+     -DarchetypeGroupId=io.radanalytics \
+     -DarchetypeVersion=${CURRENT} \
      -DinteractiveMode=false
   cd x-operator/
   mvn install
