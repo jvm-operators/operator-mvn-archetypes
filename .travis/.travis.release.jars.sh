8a93138b055b4e59ac1393f45d762ddbc67e7ed7
Jirka Kremser <jkremser@redhat.com>
2018-08-30 13:01:42 +0200

    scripts + tests

---
+++ .travis/.travis.release.jars.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -xe
+
+[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ] && LATEST=1
+
+main() {
+  if [[ "$LATEST" = "1" ]]; then
+    echo "Pushing the -SNAPSHOT artifact to sonatype maven repo."
+    releaseSnapshot
+    javadoc
+  elif [[ "${TRAVIS_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Releasing the '${TRAVIS_TAG}' maven artifacts."
+    release
+    javadoc
+  else
+    echo "Not doing the Maven release, because the tag '${TRAVIS_TAG}' is not of form x.y.z"
+    echo "and also it's not a build of the master branch"
+  fi
+}
+
+releaseSnapshot() {
+    make build-travis && mvn -s ./.travis/settings.xml clean deploy
+}
+
+release() {
+    openssl aes-256-cbc -K ${encrypted_ea794cf5410d_key} -iv ${encrypted_ea794cf5410d_iv} -in ./.travis/.signing.asc.enc -out ./signing.asc -d
+    gpg --fast-import ./signing.asc &> /dev/null
+    mvn -s ./.travis/settings.xml clean deploy -DskipLocalStaging=true -P release
+    sleep 10
+    local _repo_id=`mvn -s ./.travis/settings.xml nexus-staging:rc-list | grep "ioradanalytics".*OPEN | cut -d' ' -f2 | tail -1`
+    mvn -s ./.travis/settings.xml nexus-staging:close nexus-staging:release -DstagingRepositoryId=${_repo_id}
+}
+
+main
---
+++ .travis/.travis.test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -xe
+
+main() {
+  CURRENT=`mvn -U org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev "(^\[|Download\w+:)"`
+  mvn archetype:generate -DarchetypeArtifactId=operator-mvn-archetype \
+     -DarchetypeGroupId=io.radanalytics -DarchetypeVersion=${CURRENT} \
+     -DinteractiveMode=false
+  cd x-operator/
+  mvn install
+}
+
+main
