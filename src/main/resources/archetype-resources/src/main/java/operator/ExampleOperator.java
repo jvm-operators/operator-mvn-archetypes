96313a68568a580c5562a8804d05b01e72ac3b0b
Jirka Kremser <jkremser@redhat.com>
2018-08-30 12:04:35 +0200

    almost working prototype

--- src/main/resources/archetype-resources/src/main/java/operator/ExampleOperator.java
+++ src/main/resources/archetype-resources/src/main/java/operator/ExampleOperator.java
@@ -1,55 +1,32 @@
 package operator;
 
+import io.fabric8.kubernetes.api.model.KubernetesResourceList;
+import io.radanalytics.operator.common.AbstractOperator;
+import io.radanalytics.operator.common.Operator;
+import io.radanalytics.types.Example;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Operator(forKind = Example.class, prefix = "${groupId}")
-public class SparkClusterOperator extends AbstractOperator<SparkCluster> {
+public class ExampleOperator extends AbstractOperator<Example> {
 
     private static final Logger log = LoggerFactory.getLogger(AbstractOperator.class.getName());
 
-    private final RunningClusters clusters;
-    private KubernetesSparkClusterDeployer deployer;
-
-    public SparkClusterOperator() {
-        this.clusters = new RunningClusters();
+    public ExampleOperator() {
     }
 
-    protected void onAdd(SparkCluster cluster) {
-        KubernetesResourceList list = getDeployer().getResourceList(cluster);
-        client.resourceList(list).createOrReplace();
-        clusters.put(cluster);
+    protected void onAdd(Example example) {
+        log.info("new example has been created: {}", example);
+        // todo: implement the logic
+        // KubernetesResourceList list = ???
+        // client.resourceList(list).createOrReplace();
     }
 
-    protected void onDelete(SparkCluster cluster) {
-        String name = cluster.getName();
-        client.services().withLabels(getDeployer().getDefaultLabels(name)).delete();
-        client.replicationControllers().withLabels(getDeployer().getDefaultLabels(name)).delete();
-        client.pods().withLabels(getDeployer().getDefaultLabels(name)).delete();
-        clusters.delete(name);
+    protected void onDelete(Example example) {
+        log.info("existing example has been deleted: {}", example);
     }
 
-    protected void onModify(SparkCluster newCluster) {
-        String name = newCluster.getName();
-        String newImage = newCluster.getCustomImage();
-        int newMasters = newCluster.getMasterNodes();
-        int newWorkers = newCluster.getWorkerNodes();
-        SparkCluster existingCluster = clusters.getCluster(name);
-        if (null == existingCluster) {
-            log.error("something went wrong, unable to scale existing cluster. Perhaps it wasn't deployed properly.");
-            return;
-        }
-
-        if (existingCluster.getWorkerNodes() != newWorkers) {
-            log.info("{}scaling{} from {}{}{} worker replicas to {}{}{}", re(), xx(), ye(),
-                    existingCluster.getWorkerNodes(), xx(), ye(), newWorkers, xx());
-            client.replicationControllers().withName(name + "-w").scale(newWorkers);
-            clusters.put(newCluster);
-        }
-        // todo: image change, masters # change for k8s
-    }
-
-    public KubernetesSparkClusterDeployer getDeployer() {
-        if (this.deployer == null) {
-            this.deployer = new KubernetesSparkClusterDeployer(client, entityName, prefix);
-        }
-        return deployer;
+    protected void onModify(Example example) {
+        log.info("existing example has been modified: {}", example);
     }
 }
